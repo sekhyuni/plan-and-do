08e388fe167858bf7abe4af87e6871e2db3e629f
김석현 <sekhyuni@gmail.com>
2020-12-28 17:14:55 +0900

    MonthSelect, YearSelect Component 추가

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,29 +2,10 @@
     <div class="calendar-container">
         <div class="calendar-header">
             <!-- 월 선택 -->
-            <div class="select-box">
-                <div class="select-box__current" tabindex="1">
-                    <div class="select-box__value" v-for="(monthName, idx) in monthsName" :key="monthName.id">
-                        <input class="select-box__input" type="radio" :id="idx" :value="idx + 1" name="Month" v-if="monthName === 'January'" checked="checked" />
-                        <input class="select-box__input" type="radio" :id="idx" :value="idx + 1" name="Month" v-else/>
-    
-                        <p class="select-box__input-text">{{ monthName }}</p>
-                    </div>
-                    <img class="select-box__icon" src="http://cdn.onlinewebfonts.com/svg/img_295694.svg" alt="Arrow Icon" aria-hidden="true" />
-                </div>
-    
-                <ul class="select-box__list">
-                    <li v-for="(monthName, idx) in monthsName" :key="monthName.id">
-                        <label class="select-box__option" :for="idx" aria-hidden="aria-hidden">{{ monthName }}</label>
-                    </li>
-                </ul>
-            </div>
-
+            <MonthSelect></MonthSelect>
             <br>
-
-            <select>
-                <option v-for="year in years" :key="year.id">{{ year }}</option>
-            </select>
+            <!-- 년 선택 -->
+            <YearSelect></YearSelect>
         </div>
     
         <!-- 달력 -->
@@ -40,31 +21,26 @@
 </template>
 
 <script>
+import MonthSelect from './MonthSelect'
+import YearSelect from './YearSelect'
+
 export default {
+    components: { MonthSelect, YearSelect },
     data() {
         return {
             days: [],
             daysName: [],
-            monthsName: [],
-            years: [],
         }
     },
     methods: {
         dateInit() {
             const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
+         
             for (let i = 1; i <= 31; i++) {
                 this.days.push(i)
             }
             for (let i = 0, len = week.length; i < len; i++) {
                 this.daysName.push(week[i])
-            }
-            for (let i = 2010; i <= 2030; i++) {
-                this.years.push(i)
-            }
-            for (let i = 0, len = month.length; i < len; i++) {
-                this.monthsName.push(month[i])
             }
         },
     },
@@ -288,103 +264,6 @@ body {
             font-weight: 500;
             color: rgba(#51565d, .7);
         }
-    }
-}
-
-.select-box {
-    position: relative;
-    display: block;
-    width: 100%;
-    margin: 0 auto;
-    font-family: 'Open Sans', 'Helvetica Neue', 'Segoe UI', 'Calibri', 'Arial', sans-serif;
-    font-size: 18px;
-    color: #60666d;
-    @media (min-width: 768px) {
-        width: 70%;
-    }
-    @media (min-width: 992px) {
-        width: 50%;
-    }
-    @media (min-width: 1200px) {
-        width: 30%;
-    }
-    &__current {
-        position: relative;
-        box-shadow: 0 15px 30px -10px transparentize(#000, 0.9);
-        cursor: pointer;
-        outline: none;
-        &:focus {
-            &+.select-box__list {
-                opacity: 1; // We have to set "animation-name: none;" to make the list visible (read below how it works)
-                animation-name: none;
-                .select-box__option {
-                    cursor: pointer;
-                }
-            }
-            .select-box__icon {
-                transform: translateY(-50%) rotate(180deg);
-            }
-        }
-    }
-    &__icon {
-        position: absolute;
-        top: 50%;
-        right: 15px;
-        transform: translateY(-50%);
-        width: 20px;
-        opacity: 0.3;
-        transition: 0.2s ease;
-    }
-    &__value {
-        display: flex;
-    }
-    &__input {
-        display: none;
-        &:checked+.select-box__input-text {
-            display: block;
-        }
-    }
-    &__input-text {
-        display: none;
-        width: 100%;
-        margin: 0;
-        padding: 15px;
-        background-color: #fff;
-    }
-    &__list {
-        position: absolute;
-        width: 100%;
-        padding: 0;
-        list-style: none;
-        opacity: 0; // We need to use animation with delay.
-        // Otherwise the click event will not have time to run on label, because this element disapears immediately when .select-box__current element loses the focus.
-        // This delay will not be noticed because we set "opacity" to "0".
-        // We also use "animation-fill-mode: forwards" to make the list stay hidden.
-        animation-name: HideList;
-        animation-duration: 0.5s;
-        animation-delay: 0.5s;
-        animation-fill-mode: forwards;
-        animation-timing-function: step-start;
-        box-shadow: 0 15px 30px -10px transparentize(#000, 0.9);
-    }
-    &__option {
-        display: block;
-        padding: 15px;
-        background-color: #fff;
-        &:hover,
-        &:focus {
-            color: #546c84;
-            background-color: #fbfbfb;
-        }
-    }
-}
-
-@keyframes HideList {
-    from {
-        transform: scaleY(1);
-    }
-    to {
-        transform: scaleY(0);
     }
 }
 </style>
---
+++ src/components/YearSelect.vue
@@ -0,0 +1,29 @@
+<template>
+    <select>
+        <option v-for="year in years" :key="year.id">{{ year }}</option>
+    </select>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            years: [],
+        }
+    },
+    methods: {
+        yearInit() {
+            for (let i = 2010; i <= 2030; i++) {
+                this.years.push(i)
+            }
+        },
+    },
+    created() {
+        this.yearInit()
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
